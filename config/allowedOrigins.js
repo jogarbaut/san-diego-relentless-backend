ca884d5c5d0480220fbd043c344f33248ac48957
Jomel Bautista <jomelgbautista@gmail.com>
2023-02-15 03:16:40 -0800

    modified baseURLs for production

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,6 +1,6 @@
 const allowedOrigins = [
-  'http://localhost:8000',
-  'http://localhost:3000',
+  'https://sdr-beta-frontend.onrender.com',
+  // 'http://localhost:3000',
 ];
 
 module.exports = allowedOrigins;
